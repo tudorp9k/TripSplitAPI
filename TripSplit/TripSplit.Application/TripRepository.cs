58bb85b874a67f79de77a0ed1fb36a59125afb71
Tudor Popa <tudorp9k@gmail.com>
2024-12-15 21:35:43 +0200

    Added using

--- TripSplit/TripSplit.Application/TripRepository.cs
+++ TripSplit/TripSplit.Application/TripRepository.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TripSplit.Domain.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using TripSplit.DataAccess;
 using TripSplit.Domain;
+using TripSplit.Domain.Interfaces;
 
 namespace TripSplit.Application
 {
