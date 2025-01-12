4780e0b29e9d24bd2d9e63b9ec805a8a60b41c5d
Oltean Delia-Alexandra <oltean.delia.alexandra@gmail.com>
2025-01-12 19:07:02 +0200

    test class created

---
+++ TripSplit/TripSplit.Test/TripService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripSplit.Test
+{
+    internal class TripService_Test
+    {
+
+    }
+}
---
+++ TripSplit/TripSplit.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TripSplit.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
