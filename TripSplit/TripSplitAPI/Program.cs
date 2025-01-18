551f0a30aa4a27b7da59256fe7d0f660304907a9
Tudor Popa <tudorp9k@gmail.com>
2025-01-18 17:18:19 +0200

    Commented pdf generator in program.cs

--- TripSplit/TripSplitAPI/Program.cs
+++ TripSplit/TripSplitAPI/Program.cs
@@ -3,28 +3,28 @@ using System.Runtime.InteropServices;
 using System.Text;
 using TripSplitAPI;
 
-[DllImport("pdf_gen.dll", CallingConvention = CallingConvention.Cdecl)]
-unsafe static extern void run(byte* input, int inputLenght, byte* output, int outputLenght);
-string input = "test";
-string output = "report.pdf";
+//[DllImport("pdf_gen.dll", CallingConvention = CallingConvention.Cdecl)]
+//unsafe static extern void run(byte* input, int inputLenght, byte* output, int outputLenght);
+//string input = "test";
+//string output = "report.pdf";
 
-byte[] inputByteArray = Encoding.UTF8.GetBytes(input);
-byte[] outputByteArray = Encoding.UTF8.GetBytes(output);
+//byte[] inputByteArray = Encoding.UTF8.GetBytes(input);
+//byte[] outputByteArray = Encoding.UTF8.GetBytes(output);
 
-int inputLen = inputByteArray.Length;
-int outputLen = outputByteArray.Length;
+//int inputLen = inputByteArray.Length;
+//int outputLen = outputByteArray.Length;
 
-unsafe
-{
+//unsafe
+//{
 
-    fixed (byte* inputPtr = inputByteArray)
-    {
-        fixed (byte* outputPtr = outputByteArray)
-        {
-            run(inputPtr, inputLen, outputPtr, outputLen);
-        }
-    }
-}
+//    fixed (byte* inputPtr = inputByteArray)
+//    {
+//        fixed (byte* outputPtr = outputByteArray)
+//        {
+//            run(inputPtr, inputLen, outputPtr, outputLen);
+//        }
+//    }
+//}
 
 var builder = WebApplication.CreateBuilder(args);
 
